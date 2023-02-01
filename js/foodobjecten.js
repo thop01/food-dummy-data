2aac92e1205c4ae04d5ede07fd10fe779085aa37
thop01 <p.thong@rocmondriaan.nl>
2023-02-01 12:44:16 +0100

    init dummy data

---
+++ js/foodobjecten.js
@@ -0,0 +1,88 @@
+const breakfast = {
+    categoryName: "Breakfast",
+    startTime: "9:00",
+    endTime: "11:00",
+    dishes: [
+        {
+            dishName: "uitsmijter speciaal",
+            ingredients: "brood, 3 eieren, kaas, kip en sla",
+            price: 6.42 
+        },
+        {
+            dishName: "English breakfast",
+            ingredients: "braadworsten, tomatensaus, eieren, tijgerbrood",
+            price: 9.69
+        },
+        {
+            dishName: "Bagel Bugs",
+            ingredients: "Bagel, gedroogde krekels, meelwormen, advocado, zon tomaatjes",
+            price: 10
+        },
+        {
+            dishName: "Peanut butter jelly time",
+            ingredients: "brood, boter, jam, hagelslag",
+            price: 8
+        },
+    ],
+};
+
+const lunch = {
+    categoryName: "Lunch",
+    startTime: "11:30",
+    endTime: "14:00",
+    dishes: [
+        {
+            dishName: "Pasta Aglio Olio",
+            ingredients: [ "spaghetti", "knoflook", "Olijf olie", "pepers"],
+            price: 6.42 
+        },
+        {
+            dishName: "Smoke Beef Carbonara",
+            ingredients: [ "spaghetti", "eieren", "Olijf olie", "beef"],
+            price: 12.34
+        },
+        {
+            dishName: "Basil Pasta",
+            ingredients: [ "Penne", "bassilicum", "pitten", "pepers"],
+            price: 10
+        },
+        {
+            dishName: "Choco banana pancakes",
+            ingredients: [ "Pannekoek", "chocolade", "banaan"],
+            price: 8
+        },
+    ],
+};
+
+
+const diner = {
+    categoryName: "Diner",
+    startTime: "17:00",
+    endTime: "20:00",
+    dishes: [
+        {
+            dishName: "Garnalen dim sum",
+            ingredients: [ "bisque", "tay soi"],
+            price: 26.85 
+        },
+        {
+            dishName: "Kip rendang taco’s",
+            ingredients: [ "spasesam-sinaasappel", "komkommer", "bosui"],
+            price: 29.69
+        },
+        {
+            dishName: "Oesters rockefeller",
+            ingredients: [ "spaspinazie-chili gratin", "rode ui", "Olijf olie", "pepers"],
+            price: 24.94
+        },
+        {
+            dishName: "Millers tasting box",
+            ingredients: [ "Garnalen dim sum", "Kip rendang taco", "Oesters rockefeller"],
+            price: 90.99
+        },
+    ],
+};
+
+
+
+
